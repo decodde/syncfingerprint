bae2c4ffe8d37566f191effbcdc5a5d0e40bd418
Fasina Oreoluwa <dannyoma75@gmail.com>
2020-11-05 21:38:23 +0100

    +added recordings

---
+++ fing.py
@@ -0,0 +1,32 @@
+# coding : utf-8
+import acoustid
+import logging
+from subprocess import run, PIPE
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/audio', methods=['POST'])
+def audio():
+    f = request.data
+    with open('./audio.wav', 'wb') as audio:
+        audio.write(f)
+    print('file uploaded successfully')
+    #match("audio.wav")
+    return match("audio.wav")
+
+def match(path):
+    result = acoustid.match('HVAjaRnJuC', path)
+    for score,recording_id,title,artist in acoustid.match('HVAjaRnJuC', path):
+        print(score, artist , title)
+    return result
+    
+
+if __name__ == "__main__":
+    app.logger = logging.getLogger('audio-gui')
+    app.run(debug=True)
